260053b81faf74f28313c6136969b83f94dff03b
Ankijit2 <ankijit714@gmail.com>
2025-04-18 22:50:29 +0530

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-    apps: [
-      {
-        name: "arvan-backend",
-        script: "bash",
-        args: "-lc \"prisma generate && NODE_ENV=production node dist/index.js\"",
-        // optional: watch: false,
-        // optional: instances: 2, exec_mode: "cluster"
-      }
-    ]
-  };
-  
+// ecosystem.config.js
+export default {
+  apps: [
+    {
+      name: "arvan-backend",
+      script: "bash",
+      args: "-lc \"prisma generate && NODE_ENV=production node dist/index.js\"",
+    }
+  ]
+}
